3adc1fb8117b0e9f4923bfe04d44ee043380576e
Luis Fernando Brasil <luis4756@live.com>
2023-04-13 20:57:11 -0300

    Atualizaçao index

--- oktopus/lib/widget/index.dart
+++ oktopus/lib/widget/index.dart
@@ -9,11 +9,21 @@ class Index extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Introdução')),
-      body: ElevatedButton(
-        child: const Text('Login'),
-        onPressed: () {
-          Navigator.pushReplacementNamed(context, Rotas.login);
-        },
+      body: Center(
+        child: Row(children: [
+          ElevatedButton(
+            child: const Text('Login'),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, Rotas.login);
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Cadastrar-se'),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, Rotas.cadastroUsuario);
+            },
+          )
+        ]),
       ),
     );
   }
