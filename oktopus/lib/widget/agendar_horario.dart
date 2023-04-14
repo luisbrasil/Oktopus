acc4bb87b21d9be6fc0e8b2026dd48b4a9a7000e
Luis Fernando Brasil <luis4756@live.com>
2023-04-13 22:27:21 -0300

    Adcionado popAndPushNamed

--- oktopus/lib/widget/agendar_horario.dart
+++ oktopus/lib/widget/agendar_horario.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../rotas.dart';
+
 class AgendarHorario extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Agendar Horário")),
+      body: ElevatedButton(
+        child: const Text('Confirmar Agendamento'),
+        onPressed: () {
+          Navigator.popAndPushNamed(context, Rotas.agendamentos);
+        },
+      ),
     );
   }
 }
